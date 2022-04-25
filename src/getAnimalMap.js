18d4356af3347f3b24ab7b337a34a696e1ed781f
Rafael vetrano <rafaelcairo2010@hotmail.com>
2022-04-25 10:08:30 -0300

    requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,41 @@
+// noinspection LanguageDetectionInspection
+
 const data = require('../data/zoo_data');
 
+function getSpeciesByLocation() {
+  return data.species.reduce((acc, specie) => {
+    acc[specie.location].push(specie);
+    return acc;
+  }, { NE: [], NW: [], SE: [], SW: [] });
+}
+
+function getResidentsNames(specie, sex = null, sorted = false) {
+  const residents = specie.residents
+    .reduce((acc, resident) => {
+      if (!sex || sex === resident.sex) acc.push(resident.name);
+      return acc;
+    }, []);
+  const obj = {};
+  if (sorted) residents.sort();
+  obj[specie.name] = residents;
+  return obj;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  const output = { NE: [], NW: [], SE: [], SW: [] };
+  const speciesByLocation = getSpeciesByLocation();
+  Object.keys(speciesByLocation)
+    .forEach(
+      (key) => {
+        output[key] = speciesByLocation[key]
+          .map(
+            (specie) => (!options || !options.includeNames
+              ? specie.name
+              : getResidentsNames(specie, options.sex || null, options.sorted || false)),
+          );
+      },
+    );
+  return output;
 }
 
 module.exports = getAnimalMap;
