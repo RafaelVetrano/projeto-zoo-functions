f1b34c945e1a2ea6f5bd31ad89df15cfcb5c5ffa
Rafael vetrano <rafaelcairo2010@hotmail.com>
2022-04-25 09:44:42 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
+function categorizeEntrantAge(entrant) {
+  if (entrant.age < 18) return 'child';
+  if (entrant.age < 50) return 'adult';
+  return 'senior';
+}
+
 function countEntrants(entrants) {
-  // seu código aquii
+  return entrants.reduce((acc, entrant) => {
+    acc[categorizeEntrantAge(entrant)] += 1;
+    return acc;
+  },
+  {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const cEntrants = Object.entries(countEntrants(entrants));
+  return cEntrants.reduce((acc, entrant) => acc + entrant[1] * data.prices[entrant[0]], 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
